1802b0ae780780ecb992e2ae4651388f25873665
Hall C DAQ <cdaq@cdaql1.jlab.org>
2025-09-30 14:33:49 -0400

    Updated SHMS drift chamber plotting tool to draw line at y=0

--- CALIBRATION_checks/pdc_calib_checks/CALIB_CHECKS_pdc.py
+++ CALIBRATION_checks/pdc_calib_checks/CALIB_CHECKS_pdc.py
@@ -2,20 +2,10 @@
 
 import ROOT
 ROOT.gROOT.SetBatch(True)
+ROOT.gErrorIgnoreLevel = ROOT.kFatal
 import sys
 
-runnum = input(f"Input the run number you wish to analyze:")
-
-# try:
-#     input_root_filepath = f"/volatile/hallc/c-rsidis/cmorean/replay_pass0a/ROOTfiles/coin_replay_production_{runnum}_-1.root"
-#     file_type = "coin"
-# except FileNotFoundError:
-#     try:
-#         input_root_filepath = f"/volatile/hallc/c-rsidis/cmorean/replay_pass0a/ROOTfiles/shms_coin_replay_production_{runnum}_-1.root"
-#         file_type = "shms"
-#     except FileNotFoundError:
-#         print(f"Run {runnum} not valid for pdc calibration; skipping...")
-#         exit
+runnum = input(f"Input run number:")
 
 try:
     input_root_filepath = f"/volatile/hallc/c-rsidis/cmorean/replay_pass0a/ROOTfiles/coin_replay_production_{runnum}_-1.root"
@@ -34,20 +24,53 @@ except OSError:
         print(f"Run {runnum} not valid for pdc calibration; skipping...")
         sys.exit(1)
 
+print(f"Found {file_type} run {runnum}.  Processing...")
 
 def draw_group(tree, cards, cname, title):
     c = ROOT.TCanvas(cname, title, 1000, 800)
-    c.Divide(2, 2)
+    c.Divide(2, 2, 0.01, 0.01)
+    lines = []
 
     for i, cardnum in enumerate(cards):
-        c.cd(i+1)
+        ROOT.gStyle.SetOptStat(1100)
+        pad = c.cd(i+1)
+        pad.SetLeftMargin(0.05)
+        pad.SetRightMargin(0.05)
+        pad.SetTopMargin(0.05)
+        pad.SetBottomMargin(0.05)
         hname = f"h_{cardnum}"
         expr = f"P.dc.{cardnum}.time:P.dc.{cardnum}.wirenum >> {hname}(117,-5,110,110,-100,250)"
         tree.Draw(expr, "", "COLZ")
-        ROOT.gPad.SetRightMargin(0.15)
         h = ROOT.gPad.GetPrimitive(hname)
+
+        stats = h.GetListOfFunctions().FindObject("stats")
+        if stats:
+            stats.SetTextSize(0.02)
+            stats.SetX1NDC(0.75)
+            stats.SetX2NDC(0.95)
+            stats.SetY1NDC(0.75)
+            stats.SetY2NDC(0.95)
+            
         if h:
-            print(f"{hname} entries:", h.GetEntries())
+            # print(f"{hname} entries:", h.GetEntries()) # Uncomment this line if you wish to see number of entries printed for each card
+            xmin = h.GetXaxis().GetXmin()
+            xmax = h.GetXaxis().GetXmax()
+
+            # Drawing a highlighted, dashed red line to appear at y = 0; this is to guide the eye while reviewing calibration plots.
+            highlight = ROOT.TLine(xmin, 0, xmax, 0)
+            highlight.SetLineColor(ROOT.kWhite)
+            highlight.SetLineWidth(4)
+            highlight.SetLineStyle(2)
+            highlight.Draw("same")
+            lines.append(highlight)
+            
+            line = ROOT.TLine(xmin, 0, xmax, 0)
+            line.SetLineColor(ROOT.kRed)
+            line.SetLineWidth(2)
+            line.SetLineStyle(2)
+            line.Draw("same")
+            lines.append(line)
+    c.lines = lines
     c.Update()
     c.SaveAs(f"{cname}.png")
     return c
@@ -55,12 +78,12 @@ def draw_group(tree, cards, cname, title):
 def main():
     T = f.Get("T")
 
-    # Defining the groups
+    # Defining the cards
     us = ["1u1","1u2","2u1","2u2"]
     xs = ["1x1","1x2","2x1","2x2"]
     vs = ["1v1","1v2","2v1","2v2"]
 
-    # Draw each group
+    # Drawing each card
     draw_group(T, xs, f"{file_type}/XPLANE/{file_type}_run_{runnum}_pdc_xplane", "X planes")
     draw_group(T, us, f"{file_type}/UPLANE/{file_type}_run_{runnum}_pdc_uplane", "U planes")
     draw_group(T, vs, f"{file_type}/VPLANE/{file_type}_run_{runnum}_pdc_vplane", "V planes")
