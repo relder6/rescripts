ce4ae0e606cac3eccce3de5f262d59b4804e60bc
Ryan Elder <relder6@vols.utk.edu>
2025-11-03 11:10:42 -0500

    Making final git updates for TESTING_ROOTfiles directory

--- TESTING_ROOTfiles/EXTRACT_filenames.py
+++ TESTING_ROOTfiles/EXTRACT_filenames.py
@@ -3,8 +3,13 @@
 import os
 import re
 
-directory = "/work/hallc/c-rsidis/skimfiles/pass0"
 output_file = "filenames.txt"
+
+# ------------------------------------------------
+# Modify to suit your needs; the script will grab all file names that
+# match the patterns below.  Run numbers will be preserved in output.
+# ------------------------------------------------
+ROOTfile_directory = "/work/hallc/c-rsidis/skimfiles/pass0"
 
 patterns = [
     re.compile(r"(skimmed_hms_coin_replay_production_\d+_-1\.root)"),
@@ -12,6 +17,9 @@ patterns = [
     re.compile(r"(skimmed_coin_replay_production_\d+_-1\.root)")
 ]
 
+# ------------------------------------------------
+# Defining and executing the script's machinery.
+# ------------------------------------------------
 def extract_filenames(dirpath):
     filenames = set()
     for filename in os.listdir(dirpath):
@@ -27,7 +35,7 @@ def write_filenames(filenames, filepath):
         f.write(",".join(filenames))
 
 def main():
-    files = extract_filenames(directory)
+    files = extract_filenames(ROOTfile_directory)
     write_filenames(files, output_file)
     print(f"Wrote {len(files)} filenames to {output_file}")
 
