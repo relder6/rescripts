39c6018616fe062149ca43e7d46ec6a44674b2f2
Ryan Elder <relder6@vols.utk.edu>
2025-11-05 10:10:21 -0500

    Updating histogram generation script

--- MAKE_histos/MAKE_histos.py
+++ MAKE_histos/MAKE_histos.py
@@ -15,18 +15,53 @@ runnum = input(f"Input run number: ")
 # -----------------------------------------------------
 # File paths
 # -----------------------------------------------------
-
 skimmed_rootfile_dir = "/work/hallc/c-rsidis/skimfiles/pass0"
 skimmed_hms_pattern = f"skimmed_hms_coin_replay_production_{runnum}_-1.root"
 
 rootfile_path = f"{skimmed_rootfile_dir}/{skimmed_hms_pattern}"
 
-target = "C"
+# =====================================================================
+# Handling user inputs
+# =====================================================================
+
+selected_type = input("Enter desired run type (default HMSDIS): ").strip().lower()
+if not selected_type:
+    selected_type = f"hmsdis"
+    
+selected_beam_pass = input("Enter desired beam pass (present options: 1, 4, 5): ").strip()
+selected_beam_pass_to_energy_prefix = {"1": "2.",
+                                       "2": "4.",
+                                       "3": "6.",
+                                       "4": "8.",
+                                       "5": "10."}
+beam_prefix = selected_beam_pass_to_energy_prefix.get(selected_beam_pass)
+if not beam_prefix:
+    print(f"Unknown pass: {selected_beam_pass}.  Please try again.")
+    exit(1)
+
+selected_target = input("Enter desired target (options: C, Cu, Al, LD2, LH2, Dummy): ").strip().lower()
+selected_target_shortcut_to_target_variable = {"al":"al","al13":"al","aluminum":"al",
+                                               "c":"c","c12":"c","carbon":"c",
+                                               "cu":"cu","cu29":"cu","copper":"cu",
+                                               "opt1":"optics1","optics1":"optics1",
+                                               "opt2":"optics2","optics2":"optics2",
+                                               "d2":"ld2","ld2":"ld2",
+                                               "h2":"lh2","lh2":"lh2",
+                                               "hole":"hole","chole":"hole","c-hole":"hole",
+                                               "dummy":"dummy","dum":"dummy",
+                                               }
+selected_target_shortname = selected_target_shortcut_to_target_variable.get(selected_target)
+if not selected_target_shortname:
+    print(f"Unknown target: {selected_target}.  Please try again.")
+    exit(1)
+
+output_filepath = f"{selected_target_shortname.upper()}/{selected_type}_{selected_beam_pass}pass_{selected_target_shortname}_runs.dat" # The name and location of the output file
+
+
 
 # -----------------------------------------------------
 # Defining branches, using uproot to put them in data frames
 # -----------------------------------------------------
-
 branches = ["H_gtr_dp",
             "H_cal_etottracknorm",
             "H_gtr_ph",
