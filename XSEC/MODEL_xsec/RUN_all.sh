495e7d4353c085d15170812fb02b850a32d75e2d
Ryan Elder <relder6@vols.utk.edu>
2026-05-13 16:11:37 -0400

    Updated model xsec grabbing script.  Now it outputs to .csv directly.

--- XSEC/MODEL_xsec/RUN_all.sh
+++ XSEC/MODEL_xsec/RUN_all.sh
@@ -4,7 +4,7 @@ run_types=(hmsdis)
 beam_passes=(4 5)
 targets=(al c cu ld2 lh2)
 
-script_path="./GRAB_model.py"
+script_path="./CALC_dis_xsec.py"
 
 for run_type in "${run_types[@]}"; do
   for beam_pass in "${beam_passes[@]}"; do
